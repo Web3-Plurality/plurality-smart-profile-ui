aec92280dcdc9f92851d5bbd92677f921dcc7d4e
Zabeeh Ullah <zullah.bese15seecs@seecs.edu.pk>
2025-01-21 12:26:58 +0500

    feat: removed dashboard page

--- src/components/authSuccess.tsx
+++ src/components/authSuccess.tsx
@@ -1,28 +1,86 @@
-
-
-import CustomButtom from './customButton'
-import { getLocalStorageValueofClient } from '../utils/Helpers';
-import { CLIENT_ID } from '../utils/EnvConfig';
-import { useStepper } from '../hooks/useStepper';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { useAccount, useDisconnect } from "wagmi"
+import CustomButtom from "./customButton"
+import { connectOrbisDidPkh } from "../services/orbis/getOrbisDidPkh"
+import type { AuthUserInformation } from "@useorbis/db-sdk"
+import { setLoadingState } from "../Slice/userDataSlice"
+import { LoaderMessages } from "../utils/Constants"
+import { CLIENT_ID } from "../utils/EnvConfig"
+import { getLocalStorageValueofClient } from "../utils/Helpers"
+import { useStepper } from "../hooks/useStepper"
 
 const AuthSuccess = () => {
+    const dispatch = useDispatch()
+    const { address: metamaskAddress } = useAccount()
+    const { disconnectAsync } = useDisconnect()
     const { goToStep } = useStepper()
-    const queryParams = new URLSearchParams(location.search);
-    const clientTypeId = queryParams.get('client_id') || CLIENT_ID;
+    const queryParams = new URLSearchParams(location.search)
+    const clientId = queryParams.get("client_id") || CLIENT_ID
+    const { litSession, userDid } = getLocalStorageValueofClient(`clientID-${clientId}`)
 
-    const { litSession, userDid } = getLocalStorageValueofClient(`clientID-${clientTypeId}`)
+    useEffect(() => {
+        const connectToOris = async () => {
+            dispatch(setLoadingState({ loadingState: true, text: LoaderMessages.LIT_PROFILE_SETUP }))
+            const result: AuthUserInformation | "" | "error" | undefined = await connectOrbisDidPkh()
+            if (result === "error") {
+                dispatch(setLoadingState({ loadingState: false, text: "" }))
+            } else if (result && result.did) {
+                const existingDataString = localStorage.getItem(`clientID-${clientId}`)
+                let existingData = existingDataString ? JSON.parse(existingDataString) : {}
 
-    let isDisbaled;
+                existingData = {
+                    ...existingData,
+                    userDid: result.did,
+                }
+                localStorage.setItem(`clientID-${clientId}`, JSON.stringify(existingData))
+                dispatch(setLoadingState({ loadingState: false, text: "" }))
+            } else {
+                dispatch(setLoadingState({ loadingState: false, text: "" }))
+            }
+        }
 
+        if (!userDid) {
+            connectToOris()
+        }
+
+        const disconnectMetamask = async () => {
+            if (metamaskAddress) {
+                try {
+                    await disconnectAsync()
+                } catch (err) {
+                    console.error(err)
+                }
+            }
+        }
+
+        disconnectMetamask()
+    }, [])
+
+    useEffect(() => {
+        const isInIframe = window.self !== window.top
+        const detailsCard = document.querySelector(".details-card")
+
+        if (!isInIframe && detailsCard) {
+            detailsCard.classList.add("outside-iframe")
+        } else if (detailsCard) {
+            detailsCard.classList.remove("outside-iframe")
+        }
+    }, [])
+
+    let isDisabled
     if (!litSession) {
-        isDisbaled = userDid
+        isDisabled = userDid
     } else {
-        isDisbaled = false
+        isDisabled = false
     }
 
     return (
-        <CustomButtom text={`Let's Go`} handleClick={() => goToStep('socialConnect')} isDisable={!isDisbaled} />
+        <CustomButtom text={`Let's Go`} handleClick={() => goToStep('socialConnect')} isDisable={!isDisabled} />
     )
 }
 
 export default AuthSuccess
+
+
