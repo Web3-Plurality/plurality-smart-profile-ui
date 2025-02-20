61273be4b452da87ae37210f367a61de32cffba2
Zabeeh Ullah <zullah.bese15seecs@seecs.edu.pk>
2025-02-20 16:49:09 +0500

    refactor: minor PR review changes

--- src/components/Wallet/BalanceTab/balanceDetails.tsx
+++ src/components/Wallet/BalanceTab/balanceDetails.tsx
@@ -5,16 +5,13 @@ const BalanceDetails = ({ amount, loading, selectedNetwork }: { amount: string, 
     return (
         <div className="balance-info-2">
             <div className="address">
-                <p>Balance:           </p>
+                <p>Balance:</p>
                     {loading ? (
                         <LoadingOutlined style={{ fontSize: '16px', marginLeft: "10px" }} />
                     ) : (
                         <div className="balance">
                             <img src={selectedNetwork.icon} alt="polygon" />
                             <span>{amount} {selectedNetwork.token}</span>
-                            {/* <span className="balance-info">
-                               
-                            </span> */}
                         </div>
                     )}
             </div>
